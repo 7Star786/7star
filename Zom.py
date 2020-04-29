98379ccbc926de68d21380af5124f70182884a36
7Star786 <64490812+7Star786@users.noreply.github.com>
2020-04-30 02:02:44 +0500

    Update Zom.py

--- Zom.py
+++ Zom.py
@@ -116,8 +116,8 @@ print  """\033[1;91m=======================================
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "Ami"
-CorrectPassword = "Ami"
+CorrectUsername = "7star"
+CorrectPassword = "7star"
 
 loop = 'true'
 while (loop == 'true'):
